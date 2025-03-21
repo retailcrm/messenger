45c2f1aaf52eef7de3a131e6749d450a8ce3881d
Neur0toxine <pashok9825@gmail.com>
2025-03-21 14:20:22 +0300

    ci & examples fixes

--- examples/linked-account/main.go
+++ examples/linked-account/main.go
@@ -84,7 +84,8 @@ func main() {
 			text = "You've been logged out of your account."
 		}
 
-		if _, err := r.Text(text, messenger.ResponseType, ""); err != nil {
+		if _, err := r.Text(text, messenger.ResponseType,
+			"", messenger.NotificationRegularType); err != nil {
 			log.Println("Failed to send account linking feedback")
 		}
 	})
@@ -134,7 +135,8 @@ func logoutButton(r *messenger.Response) error {
 
 // greeting salutes the user.
 func greeting(p messenger.Profile, r *messenger.Response) error {
-	_, err := r.Text(fmt.Sprintf("Hello, %v!", p.FirstName), messenger.ResponseType, "")
+	_, err := r.Text(fmt.Sprintf("Hello, %v!", p.FirstName), messenger.ResponseType,
+		"", messenger.NotificationRegularType)
 	return err
 }
 
@@ -156,7 +158,7 @@ func help(p messenger.Profile, r *messenger.Response) error {
 		},
 	}
 
-	_, err := r.TextWithReplies(text, replies, messenger.ResponseType, "")
+	_, err := r.TextWithReplies(text, replies, messenger.ResponseType, "", messenger.NotificationRegularType)
 	return err
 }
 
